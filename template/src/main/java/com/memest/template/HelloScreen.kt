8a67792128e04b073725c9bd9ea0360014df9f05
ngocji <dngocx@gmail.com>
2023-12-20 09:18:23 +0700

    Update

--- template/src/main/java/com/memest/template/HelloScreen.kt
+++ template/src/main/java/com/memest/template/HelloScreen.kt
@@ -3,7 +3,6 @@ package com.memest.template
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
-import androidx.car.app.model.ActionStrip
 import androidx.car.app.model.Pane
 import androidx.car.app.model.PaneTemplate
 import androidx.car.app.model.Row
@@ -13,17 +12,14 @@ class HelloScreen(carContext: CarContext) : Screen(carContext) {
     private var countClick = 0
     override fun onGetTemplate(): Template {
         val row = Row.Builder().setTitle("Click count: $countClick").build()
-        val pane = Pane.Builder().addRow(row).build()
-
-        val actionStrip = ActionStrip.Builder()
+        val pane = Pane.Builder().addRow(row)
             .addAction(Action.Builder()
                 .setTitle("Click")
                 .setOnClickListener { onClick() }
-                .build())
-            .build()
+                .build()).build()
+
         return PaneTemplate.Builder(pane)
             .setHeaderAction(Action.APP_ICON)
-            .setActionStrip(actionStrip)
             .build()
     }
 
